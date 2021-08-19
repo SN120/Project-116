c211e90acf87bb95c6fb521fbd9c46c753bdcc94
SN120 <79451292+SN120@users.noreply.github.com>
2021-08-19 17:14:34 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
+var nose_x = "";
+var nose_y = "";
+var clown_nose = "";
 function preload(){
+    clown_nose = loadImage("stach.png");
 }
 function setup(){
     canvas = createCanvas(400, 300);
@@ -12,17 +16,20 @@ function setup(){
 }
 function draw(){
     image(video, 0, 0, 400, 300);
+    image(clown_nose, nose_x, nose_y, 40, 40);
 }
 function Snapshot(){
     save('myFilterImage.png');
 }
 function modelLoaded(){
-    console.log("Posenet started");
+    console.log("Posenet initialised");
 }
 function gotPoses(result){
     if(result.length > 0){
         console.log(result);
-        console.log("Nose x = "+ result[0].pose.nose.x);
-        console.log("Nose y = "+ result[0].pose.nose.y);
+        nose_x = result[0].pose.nose.x - 18;
+        nose_y = result[0].pose.nose.y - 5;
+        console.log("Nose x = "+ nose_x);
+        console.log("Nose y = "+ nose_y);
     }
 }
